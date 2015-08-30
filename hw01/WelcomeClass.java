7909f9746f3c6b87b0bcc36c8f6bd3e79b03aa6d
mak619 <mak619@lehigh.edu>
2015-08-30 21:38:31 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//Brian Chen
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method 
+  public static void main(String[] args) {
+      
+//  print the statement with box around it 
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-M--A--K--6--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My first week at Lehigh has been amazing so far. I can't wait to see and learn about the various programs Lehigh has to offer...no pun intended.");
+    
+    //test/testtest
+   }
+}
