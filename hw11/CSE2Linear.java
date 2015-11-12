792f9b87314bde12cc7a0e5b9012f541e15e8ef5
mak619 <mak619@lehigh.edu>
2015-11-12 22:27:01 +0000

    updated CSE2Linear

--- hw11/CSE2Linear.java
+++ hw11/CSE2Linear.java
@@ -1,38 +1,17 @@
 // Maryam Athar Khan
 // Homework 11
-// Due date is November 10, 2015
+// Due date is November 12, 2015
 // This program is designed to give us practice in searching single dimensional arrays
 
 import java.util.Scanner; // import Scanner class
 import java.util.Random; // import Random class
 
 public class CSE2Linear { // define the class
-    public static void sorted(int[] array) { // define the sorted method
-        for (int i=0; i<array.length-1;i++) { // loop runs (length of the array -1) times
-            int currentMin=array[i]; // allocate value at i to minimum
-            int minIndex=i; // index at which minimum value is i 
-            for (int j=i+1;j<array.length;j++) { // loop runs to check if it is in increasing order
-                if (currentMin>array[j]) { // if your min is grater than the value at j or the next value
-                    currentMin=array[j]; // assign the value at j to min
-                    minIndex=j; // assign the minimum index to be j
-                } // end of if statement
-            } // end of j for loop
-            if (minIndex!=i) { // thus if you min index is not equal to i, meaning it has changed
-                array[minIndex]=array[i]; // assign the previous min value to value at j
-                array[i]=currentMin; // and assign new min value to index i 
-            } // end of if statement
-        } // end of for loop
-        System.out.println("Sorted:"); // print statement
-        for (int z=0; z<array.length; z++) { // loop runs to print out the sorted array
-            System.out.print(array[z] + " "); // print out the array
-        } // end of loop
-        System.out.println(""); // move to next line
-    } // end of sorted method
     public static void binarySearch(int[] array, int input) { // define the binarySearch method
         int low=0; // assign low to be index 0
         int high=array.length-1; // assign high to be the last index
         boolean found=false; // declare found and assign false to it 
-        int i=0; // i stands for the number of iterations 
+        int i=0; // i stands for the number of comparisons 
         while (high>=low) { // loop runs whilst high is greater than or equal to low
             i++; // increase i by 1
             int mid=(low+high)/2; // declare and find the mid index
@@ -41,7 +20,7 @@ public class CSE2Linear { // define the class
             } // end of if statement
             else if (input==array[mid]) { // else if input is equal to the value at mid index
                 found=true; // assign true to found
-                System.out.println(input +" was found in the list with " +i+" iterations"); // print out statement
+                System.out.println(input +" was found in the list with " +i+" comparisons"); // print out statement
                 break; // break out of the loop
             } // end of else if statement
             else { // otherwise if input is greater than the value at mid index
@@ -49,7 +28,7 @@ public class CSE2Linear { // define the class
             } // end of else statement
         } // end of while loop 
         if (found==false) { // if input was not found
-            System.out.println(input +" was not found in the list with "+i+" iterations"); // print statement
+            System.out.println(input +" was not found in the list with "+i+" comparisons"); // print statement
         } // end of if statement
     } // end of binarySearch method
     public static int[] scramble(int[] array) { // define the scramble method
@@ -76,18 +55,18 @@ public class CSE2Linear { // define the class
         return scrambled; // return scrambled array
     } // end of scramble method
     public static void linearSearch(int[] array, int input) { // define linearSearch method
-        int z=0; // z represents the number of iterations
+        int z=0; // z represents the number of comparisons
         boolean found=false; // declare found and assign false to it
         for (int i=0; i<array.length;i++) { // loop runs the length of the array
             z++; // increase z by 1 
             if (input==array[i]) { // if input is equal to the value at i
                 found=true; // assign true to found
-                System.out.println(input+ " was found in the list with "+z+" iterations"); // print out statement
+                System.out.println(input+ " was found in the list with "+z+" comparisons"); // print out statement
                 break; // break and exit the loop
             } // end of if statement
         } // end of for loop
         if (found==false) { // if not found
-            System.out.println(input + " was not found in the list with "+z+" iterations"); // print out statement
+            System.out.println(input + " was not found in the list with "+z+" comparisons"); // print out statement
         } // end of if statement
     } // end of linearSearch method
     public static void main(String[] args) { // define the main method 
@@ -116,54 +95,40 @@ public class CSE2Linear { // define the class
                 } // end of while loop to check if it is within range or not 
                 noErrors=true; // assign true to noErrors
                 finalGrades[i]=x; // assign x to the i index of finalGrades
+                while (i>0) { // when index is greater than 0
+                  if (finalGrades[i]>=finalGrades[i-1]) { // if the value at the i index is greater than the value at the index before it 
+                      noErrors=true; // assign true to noErrors
+                      break; // break out of the loop
+                  } // end of if statement
+                  else { // otherwise
+                      noErrors=false; // assign false to noErrors
+                     System.out.print("Error. Please enter an integer that is greater than or equal to the last int: "); // display error message
+                     while (myScanner.hasNextInt()==false) { // loop runs if input is not an integer
+                        System.out.print("Error. Please only enter an integer: "); // display error message
+                         String junk=myScanner.next(); // collect input that is not an integer
+                    } // end of while loop
+                    x=myScanner.nextInt(); // assign user's input to x 
+                    while(x<0 || x>100) { // loop runs if x is out of the range
+                    System.out.print("Error. Please enter an integer between 0-100: "); // display error message
+                    while (myScanner.hasNextInt()==false) { // loop runs if input is not an integer
+                        System.out.print("Error. Please only enter an integer: "); // display error message
+                        String junk=myScanner.next(); // collect user's junk
+                    } // end of while loop that checks for integer  
+                    x=myScanner.nextInt(); // assign user's input to x
+                    } // end of while loop that checks for range
+                     finalGrades[i]=x; // assign input to the i index of the finalGrades array
+                  } // end of else statement
+               } // end of while (i>0) loop 
             } // end of for loop
             if (noErrors==true) { // if no errors
                 break; // break and exit the loop
             } // end of if statement
         } // end of if statement
-        /* This whole part has been commented out because the third error and the sorting method contradict each other. 
-        I was told to ignore the third error and only work with the sorting method. However, if we were to use the third
-        error, it would be like this. 
-        for (int i=0; i<finalGrades.length; i++) {
-            while (myScanner.hasNextInt()==false) {
-                System.out.print("Error. Please only enter an integer: ");
-                String junk=myScanner.next();
-            }
-            int x=myScanner.nextInt();
-            while(x<0 || x>100) {
-                System.out.print("Error. Please enter an integer between 0-100: ");
-                while (myScanner.hasNextInt()==false) {
-                    System.out.print("Error. Please only enter an integer: ");
-                    String junk=myScanner.next();
-                }
-                x=myScanner.nextInt();
-            }
-            finalGrades[i]=x;
-              while (i>0) {
-                  if (finalGrades[i]>=finalGrades[i-1]) {
-                    break;
-                  }
-                  else {
-                     System.out.println("Error. Please enter an integer that is greater than or equal to the last int: ");
-                     while (myScanner.hasNextInt()==false) {
-                        System.out.print("Error. Please only enter an integer: ");
-                         String junk=myScanner.next();
-                    }
-                    x=myScanner.nextInt();
-                    while(x<0 || x>100) {
-                    System.out.print("Error. Please enter an integer between 0-100: ");
-                    while (myScanner.hasNextInt()==false) {
-                        System.out.print("Error. Please only enter an integer: ");
-                        String junk=myScanner.next();
-                    }   
-                    x=myScanner.nextInt();
-                    }
-                     finalGrades[i]=x;
-                  }
-               }
-               }*/ 
-               // end of comment
-        sorted(finalGrades); // call sorted method and run in with finalGrades
+        System.out.println("Final Grades: "); // print the final input array
+        for (int i=0; i<finalGrades.length; i++) { // loop run the length of the array
+            System.out.print(finalGrades[i] + " "); // print out the array
+        } // end of for loop
+        System.out.println(); // move to next line 
         System.out.print("Enter a grade to search for: "); // prompt user to input
         while (myScanner.hasNextInt()==false) { // loop runs to check if input is an integer
                     System.out.print("Error. Please only enter an integer: "); // display an error message
